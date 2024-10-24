10e6d9f767e02dd9ac1c779b1b2dd19c10915b8e
RYAN RODRIGUES SOARES <55926354862@info.local>
2024-10-24 16:52:52 -0300

    Trabalhando com c-sharp e array

---
+++ Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿//cria um array tamanho 3 (de 0 a 2)
+string[] carros = new string[3];
+
+for (int i = 0; i < carros.Length; i++)
+{
+Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+carros[i] = Console.ReadLine()!;
+    
+}
+// Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+// carros[1] = Console.ReadLine()!;
+
+// Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+// carros[2] = Console.ReadLine()!;
+
+Console.WriteLine($"O carro cadastrado é {carros[0]}, {carros[1]}, {carros[2]}");
---
+++ desafioArrays01/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] numeros = new int [6];
+int par = 0;
+int impar = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Digite um {i + 1}º número");
+    int numeroDigitado = int.Parse(Console.ReadLine()!);   
+
+    if (numeroDigitado %2 == 0)
+    {
+        par++;
+    }
+    else
+    {
+        impar++;
+    }
+}
+
+Console.WriteLine($"Existem {par} números pares");
+Console.WriteLine($"Existem {impar} números impares");
